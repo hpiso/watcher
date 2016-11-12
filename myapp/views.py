26ab70def88dc518777a9fd756d83dbe1396ba4d
Hugo Piso <hugopiso@yahoo.com>
2016-11-12 15:59:13 -0500

    some fix

--- myapp/views.py
+++ myapp/views.py
@@ -5,10 +5,6 @@ from myapp.models import Object
 from django.utils import timezone
 
 def home(request):
-
-    o = Object(mac_address='74:2F:68:19:85:70', date_time=timezone.now())
-    o.save()
-
     objects = Object.objects.all()
 
     return render(request, 'myapp/index.html', {'objects' : objects})
