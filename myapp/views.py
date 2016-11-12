9c90d4d2898390fb06e717ef281f361680ba0e45
Hugo Piso <hugopiso@yahoo.com>
2016-11-12 15:49:41 -0500

    add time

--- myapp/views.py
+++ myapp/views.py
@@ -2,8 +2,13 @@ from django.http import HttpResponse
 from django.shortcuts import render
 import nmap
 from myapp.models import Object
+from django.utils import timezone
 
 def home(request):
+
+    o = Object(mac_address='74:2F:68:19:85:70', date_time=timezone.now())
+    o.save()
+
     objects = Object.objects.all()
 
     return render(request, 'myapp/index.html', {'objects' : objects})
